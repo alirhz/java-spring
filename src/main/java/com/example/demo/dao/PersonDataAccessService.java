28fd63583c37138b0d229dd5cc529167f1e2cf7e
Ali <razzaghi.ali79@gmail.com>
2021-09-18 10:12:02 +0430

    change dao address

--- src/main/java/com/example/demo/dao/PersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/PersonDataAccessService.java
@@ -3,6 +3,7 @@ package com.example.demo.dao;
 import com.example.demo.model.Person;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -10,9 +11,12 @@ import java.util.UUID;
 @Repository("postgres")
 public class PersonDataAccessService implements PersonDao {
 
+    private static List<Person> DB = new ArrayList<>();
+
     @Override
     public int insertPerson(UUID id, Person person) {
-        return 0;
+        DB.add(new Person(id, person.getName()));
+        return 1;
     }
 
     @Override
@@ -22,16 +26,32 @@ public class PersonDataAccessService implements PersonDao {
 
     @Override
     public Optional<Person> selectPersonById(UUID Id) {
-        return Optional.empty();
+        return DB.stream()
+                .filter(person -> person.getId()
+                        .equals(Id))
+                .findFirst();
     }
-
     @Override
     public int deletedPersonById(UUID Id) {
-        return 0;
+        Optional<Person>personMaybe = selectPersonById(Id);
+        if (personMaybe.isEmpty()) {
+            return 0;
+        }
+        DB.remove(personMaybe.get());
+        return 1;
     }
 
     @Override
-    public int updatePersonById(UUID Id, Person person) {
-        return 0;
+    public int updatePersonById(UUID Id, Person update) {
+        return selectPersonById(Id)
+                .map(person -> {
+                    int indexOfPersonToUpdate = DB.indexOf(person);
+                    if(indexOfPersonToUpdate >= 0) {
+                        DB.set(indexOfPersonToUpdate, new Person(Id, update.getName()));
+                        return 1;
+                    }
+                    return 0;
+                })
+                .orElse(0);
     }
 }
